3a97d3c1db39bcd0e646cb2a52e1a95bdf5054f2
112frosta <112frosta@proton.me>
2024-02-19 15:25:27 +0100

    Add form and validation logic to Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,69 @@
+"use client";
+
+import { SubmitHandler, useForm } from "react-hook-form";
+import { auth } from "./actions";
+
+interface FormFields {
+  email: string;
+  password: string;
+}
+
 export default function Page() {
-  return <div>Page.tsx</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>();
+
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    await auth(data);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <div>
+          <label htmlFor="email">Email</label> <br />
+          <input
+            {...register("email", {
+              required: "Email is required",
+            })}
+            type="email"
+            id="email"
+            className={`  px-4 py-2 mt-2 transition-all duration-150 border rounded-lg outline-none bg-slate-800 border-slate-700 ${
+              errors.email ? "focus:outline-red-700" : "focus:outline-blue-700"
+            }`}
+          />
+          <div className="mt-2 text-sm font-medium text-red-600">
+            {errors.email && errors.email.message}
+          </div>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label> <br />
+          <input
+            {...register("password", {
+              required: "Password is required",
+            })}
+            type="password"
+            id="password"
+            className={`  px-4 py-2 mt-2 transition-all duration-150 border rounded-lg outline-none bg-slate-800 border-slate-700 ${
+              errors.password
+                ? "focus:outline-red-700"
+                : "focus:outline-blue-700"
+            }`}
+          />
+          <div className="mt-2 text-sm font-medium text-red-600">
+            {errors.password && errors.password.message}
+          </div>
+        </div>
+        <button
+          disabled={isSubmitting}
+          className="w-full px-4 py-2 font-medium transition-all duration-150 bg-blue-700 rounded-lg hover:bg-blue-800 disabled:bg-blue-800"
+          type="submit"
+        >
+          {isSubmitting ? "Submitting..." : "Submit"}
+        </button>
+      </form>
+    </div>
+  );
 }
